0a9affc0dc17dde5eba06dc2a7df2140bbe94995
c3c1l1a <cesswangumukima@gmail.com>
2022-11-16 16:20:50 +0800

    Get login for data

--- src/components/auth/login-form.js
+++ src/components/auth/login-form.js
@@ -1,12 +1,23 @@
 import React from 'react';
-
+import { useDispatch } from 'react-redux';
+import { login } from '../../redux/auth/login';
 import './auth.css';
 
 function LoginForm() {
+  const dispatch = useDispatch();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formObject = new FormData(e.target);
+    const data = Object.fromEntries(formObject.entries());
+
+    dispatch(login(data));
+  };
   return (
-    <div className="form-container">
+    <div className="form-container" onSubmit={onSubmit}>
       <form className="auth-form">
-        <input className="form-input" type="text" placeholder="Username" name="username" />
+        <input className="form-input" type="email" placeholder="Email" name="email" />
+        <input className="form-input" type="password" placeholder="Password" name="password" />
         <button className="submit-button" type="submit">Login</button>
       </form>
     </div>
--- src/redux/auth/login.js
+++ src/redux/auth/login.js
@@ -5,8 +5,14 @@ const initialState = [];
 
 export const login = createAsyncThunk(
   LOGIN,
-  async () => {
-    const response = await fetch('http://localhost:3001/login/');
+  async (formData) => {
+    const response = await fetch('http://localhost:3001/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
     response.data = await response.json();
 
     return response.data;
